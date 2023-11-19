69d3503bf7f3861c7d2a4a3b67880b932255413e
Andras E <rohadtsajt@gmail.com>
2023-11-19 11:40:24 +0000

    test

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -18,6 +18,7 @@ export function Counter() {
   const [calcAmount, setCalcAmount] = useState("");
 
   function handleAddClick(e) {
+    console.log(calcAmount);
     if (calcAmount) {
       const lastOp = e.target.value;
       dispatch(addition(Number(calcAmount)));
@@ -55,13 +56,16 @@ export function Counter() {
     setCalcAmount("");
   }
 
+  if (lastOpterator){
   return (
     <div className={styles.row}>
       {/* <h4>{calcArray || 0} </h4> */}
       <h2>
         {userInput} {lastOpterator}
       </h2>
-      <h4>{count} </h4>
+
+
+      <h4> {count}  </h4>
       <div className={styles.row}>
         <hr />
         <div>
@@ -149,4 +153,102 @@ export function Counter() {
       </div>{" "}
     </div>
   );
+} else {
+  return (
+    <div className={styles.row}>
+    {/* <h4>{calcArray || 0} </h4> */}
+    <h2>
+      {userInput} 
+    </h2>
+
+
+    <h4> { userInput } </h4>
+    <div className={styles.row}>
+      <hr />
+      <div>
+        {/* 1st row */}
+        <div className="row">
+          <button>S</button>
+          <button>AC</button>
+          <button>%</button>
+          <button
+            aria-label="Subtraction calc"
+            value="/"
+            onClick={handleSubClick}
+          >
+            ÷
+          </button>
+        </div>
+        {/* 2nd row  */}
+        <div className="row">
+          <button value="7" onClick={handleNumClick}>
+            7
+          </button>
+
+          <button value="8" onClick={handleNumClick}>
+            8
+          </button>
+          <button value="9" onClick={handleNumClick}>
+            9
+          </button>
+          <button
+            aria-label="Multiplication calc"
+            onClick={() => dispatch(multiplication(Number(calcAmount) || 0))}
+          >
+            s
+          </button>
+        </div>
+        {/* 3rd row */}
+        <div className="row">
+          <button value="4" onClick={handleNumClick}>
+            4
+          </button>
+          <button value="5" onClick={handleNumClick}>
+            5
+          </button>
+          <button value="6" onClick={handleNumClick}>
+            6
+          </button>
+          <button
+            aria-label="Decrementation calc"
+            value="-"
+            onClick={handleDecrClick}
+          >
+            -
+          </button>
+        </div>
+        {/* 4th row */}
+        <div className="row">
+          <button value="1" onClick={handleNumClick}>
+            1
+          </button>
+          <button value="2" onClick={handleNumClick}>
+            2
+          </button>
+          <button value="3" onClick={handleNumClick}>
+            3
+          </button>
+          <button
+            aria-label="Addition calc"
+            value="+"
+            onClick={handleAddClick}
+          >
+            +
+          </button>
+        </div>
+        {/* last row */}
+        <div className="row">
+          <button value="0" onClick={handleNumClick}>
+            0
+          </button>
+          <button onClick="dis('.')">.</button>
+          <button className={styles.doublebutton} onClick="solve()">
+            =
+          </button>
+        </div>
+      </div>
+    </div>{" "}
+  </div>
+  )
+}
 }
