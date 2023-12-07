872f1cee807489a7b3eac310890ad56a8d34b366
Andras E <75881631+AndrasE@users.noreply.github.com>
2023-12-07 09:34:41 +0000

    Merge branch 'main' of https://github.com/AndrasE/React-redux-calculator

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -294,12 +294,12 @@ export function Counter(props) {
     <div>
       <div className={styles.calcContainer}>
         <div className={styles.screensContainer}>
-          <div>
-            <Textfit mode="single" max="22">
+          <div className={styles.screenOne}>
+            <Textfit mode="single" max="24">
               {equalOp || firstInput || ". . ."} {!equalOp && operator} 
             </Textfit>
           </div>
-          <div>
+          <div className={styles.screenTwo}>
             <Textfit mode="single" max="44">
               {userInput || 0}
             </Textfit>
