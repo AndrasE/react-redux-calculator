17ab6fd8335a18ffa434a394757bb33bc9637ee5
Andras E <75881631+AndrasE@users.noreply.github.com>
2023-12-26 15:48:20 +0000

    moreabt

--- src/components/MoreAbout.js
+++ src/components/MoreAbout.js
@@ -38,31 +38,21 @@ function AboutMoreHeader() {
 function Introduction() {
   return (
     <div className={styles.moreAboutContainer}>
-      <p>Hi there, I hope you enjoyed my app.
-        At first me idea was to have a basic calculator that uses a bit of Redux
-        with React, but after some time I decided to recreate the Windows
-        calculator logic and display the numbers in a more dynamic way. This has
-        taken some time, but I have succeded recreating/mimicing the algorithm
-        and I`m fairly proud for this as I had no help, but my own imagination.{" "}
+      <p>Here I`ll share of a few things I learnt creating this application.
+        Why and how it came about; what were the challanges and of some the takeaways. {" "}
       </p>
-      <p>Hi there, I hope you enjoyed my app.
-        At first me idea was to have a basic calculator that uses a bit of Redux
-        with React, but after some time I decided to recreate the Windows
-        calculator logic and display the numbers in a more dynamic way. This has
-        taken some time, but I have succeded recreating/mimicing the algorithm
-        and I`m fairly proud for this as I had no help, but my own imagination.{" "}
-      </p><p>Hi there, I hope you enjoyed my app.
-        At first me idea was to have a basic calculator that uses a bit of Redux
-        with React, but after some time I decided to recreate the Windows
-        calculator logic and display the numbers in a more dynamic way. This has
-        taken some time, but I have succeded recreating/mimicing the algorithm
-        and I`m fairly proud for this as I had no help, but my own imagination.{" "}
-      </p><p>Hi there, I hope you enjoyed my app.
-        At first me idea was to have a basic calculator that uses a bit of Redux
-        with React, but after some time I decided to recreate the Windows
-        calculator logic and display the numbers in a more dynamic way. This has
-        taken some time, but I have succeded recreating/mimicing the algorithm
-        and I`m fairly proud for this as I had no help, but my own imagination.{" "}
+      <p>First of all, Redux was not necessary for this project. Its a state managment
+        libary that allows you to manage your app's state in a single place and keep 
+        changes in your app more predictable and traceable. This is mostly used in
+        more complex applications, but after my inital studies my
+        purpose was to actively use therefore I inculded it in this application.
+      </p><p>I spent quite some time to establish the logic behind the calculator 
+        as I didn`t want create only a basic calculator, I wanted to recreate the 
+        windows calculator behavour. It has a dynamic flow which I think make it
+        more elegant. Of course it comes with a cost on complexity. 
+      </p><p>The reason being is that the windows calculator has two rows and sometimes
+        the to rows doing their own things separately which can display a number of things. 
+        Here is a basic example to understand the logic:
       </p><p>Hi there, I hope you enjoyed my app.
         At first me idea was to have a basic calculator that uses a bit of Redux
         with React, but after some time I decided to recreate the Windows
