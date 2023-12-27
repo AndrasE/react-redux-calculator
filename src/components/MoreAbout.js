12e9993c59de3317cb54b6eb7401299ea12e219c
Andras E <rohadtsajt@gmail.com>
2023-12-27 18:05:52 +0000

    img swaps and moreabout text update

--- src/components/MoreAbout.js
+++ src/components/MoreAbout.js
@@ -2,15 +2,16 @@ import React from "react";
 import react_logo from "../images/react_logo.svg";
 import axios_logo from "../images/axios_logo.svg";
 import redux_logo from "../images/redux_logo.svg";
+import redux_data_flow from "../images/redux_data_flow.gif";
+import table_example from "../images/table_example.png"
 import styles from "../components/styles.module.css";
+
 
 function AboutMoreHeader() {
   return (
     <div>
       <div>
-        <h1>
-          Welcome to more about
-        </h1>
+        <h1>Welcome to more about</h1>
       </div>
       <div className={styles.logosContainer}>
         <img
@@ -38,46 +39,37 @@ function AboutMoreHeader() {
 function Introduction() {
   return (
     <div className={styles.moreAboutContainer}>
-      <p>Here I`ll share of a few things I learnt creating this application.
-        Why and how it came about; what were the challanges and of some the takeaways. {" "}
+      <p className={styles.moreAboutTitle}>Introduction</p>
+      <p>
+        Here I`ll share a few things I learnt creating this application. Why and
+        how it came about; what were the challenges and of some the takeaways.
       </p>
-      <p>First of all, Redux was not necessary for this project. Its a state managment
-        libary that allows you to manage your app's state in a single place and keep 
-        changes in your app more predictable and traceable. This is mostly used in
-        more complex applications, but after my inital studies my
-        purpose was to actively use therefore I inculded it in this application.
-      </p><p>I spent quite some time to establish the logic behind the calculator 
-        as I didn`t want create only a basic calculator, I wanted to recreate the 
-        windows calculator behavour. It has a dynamic flow which I think makes it
-        more elegant. Of course, it comes with a cost on complexity. 
-      </p><p>The reason being is that the windows calculator has two rows and sometimes
-        the to rows doing their own things seemingly separately which can display a number of things. 
-        Here is a basic example to understand the logic:
-      </p><p>One of the reason for the windows caalculator elegancy is that
-        you can pass a number and an operation and an othernumber, but you 
-        can continue with new operations while its accordingly update the screens
-        and conduct the previous operation. This is quite a predaciment. 
-        {" "}
-      </p><p>Hi there, I hope you enjoyed my app.
-        At first me idea was to have a basic calculator that uses a bit of Redux
-        with React, but after some time I decided to recreate the Windows
-        calculator logic and display the numbers in a more dynamic way. This has
-        taken some time, but I have succeded recreating/mimicing the algorithm
-        and I`m fairly proud for this as I had no help, but my own imagination.{" "}
-      </p><p>Hi there, I hope you enjoyed my app.
-        At first me idea was to have a basic calculator that uses a bit of Redux
-        with React, but after some time I decided to recreate the Windows
-        calculator logic and display the numbers in a more dynamic way. This has
-        taken some time, but I have succeded recreating/mimicing the algorithm
-        and I`m fairly proud for this as I had no help, but my own imagination.{" "}
+      <p className={styles.moreAboutTitle}>Redux(Toolkit)</p>
+      <p>
+        First of all, Redux was not necessary for this project. Its a state
+        management library that allows you to manage your app's state in a
+        single place and keep changes in your app more predictable and
+        traceable. This is mostly used in more complex applications, but after
+        my initial studies my purpose was to actively use it, therefore I
+        included it in this application.
       </p>
-   
+      <img className={styles.moreAboutImg} style={{maxWidth: 350}} src={redux_data_flow} alt="redux_data_flow"></img>
+      <p className={styles.moreAbouTitle}>Windows calculator</p>
+      <p>
+        I spent quite some time establishing the logic behind the calculator as
+        I didn't want to create only a basic calculator, I wanted to recreate
+        the windows calculator behaviour. It has a dynamic flow which I think
+        makes it more elegant. Of course, it comes with a cost of complexity.
+        The reason being is that the windows calculator has two displays and
+        sometimes the two do their own things seemingly separately which
+        can display a number among other things. You can pass a number and an operation and another number, but you can continue with new operations while its accordingly update the screens and conduct the previous operation while using the new calculated value. This is quite a predicament. Here is a basic example to understand the logic:
+      </p>
+      <img className={styles.moreAboutImg} src={table_example} alt="table_example"></img>
     </div>
   );
 }
 
 const MoreAbout = ({ showMoreAbout, toggleShowMoreAbout }) => {
-  
   if (!showMoreAbout) return null;
 
   return (
